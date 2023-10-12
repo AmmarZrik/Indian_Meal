b1a38b9645afcf57aab6944dc9e7c0f07d729708
Ammar Zrik <ammarzriek9999@gmail.com>
2023-10-12 21:13:54 +0300

    add navigationBoottom to the homeActivity and end splashScreen

--- app/src/main/java/com/example/indianmeal/HomeActivity.kt
+++ app/src/main/java/com/example/indianmeal/HomeActivity.kt
@@ -1,13 +1,17 @@
 package com.example.indianmeal
 
+import android.opengl.Visibility
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.indianmeal.databinding.ActivityMainBinding
 import com.example.indianmeal.fragments.SplashScreen
 lateinit var binding :ActivityMainBinding
-
+val handler= Handler()
+val home=Home()
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,16 +22,33 @@ class HomeActivity : AppCompatActivity() {
 
     private fun setup() {
         val splashScreen=SplashScreen()
-        changeFragment(splashScreen)
+
+        setFragment(splashScreen)
+        replaceSplashScreen()
+
+
+
+    }
+
+    private fun replaceSplashScreen() {
+        handler.postDelayed({setFragment(home)
+            binding.bottomNavigation.visibility= View.VISIBLE},
+            2000)
+    }
+
+    private fun initial(){
 
     }
 
 
 
-    private fun changeFragment(fragment:Fragment){
+    private fun setFragment(fragment:Fragment){
         val transaction=supportFragmentManager.beginTransaction()
         transaction.replace(R.id.fragment,fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+        if(fragment is SplashScreen)
+            binding.bottomNavigation.visibility= View.INVISIBLE
+
     }
 }
