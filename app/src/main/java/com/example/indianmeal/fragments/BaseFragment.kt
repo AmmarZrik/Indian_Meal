7dab23dcb6969d292a56d6db93faa0dd792eb342
Molham <molhamalnaab@gmail.com>
2023-10-18 14:57:00 +0300

    add fragment search and signup and about

--- app/src/main/java/com/example/indianmeal/fragments/BaseFragment.kt
+++ app/src/main/java/com/example/indianmeal/fragments/BaseFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+
 
 abstract class BaseFragment<VB : ViewBinding> : Fragment() {
     lateinit var binding: VB
